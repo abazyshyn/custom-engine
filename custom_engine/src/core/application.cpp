a87de643b2f21076aa8687cbdce1d565cd0cea09
abazyshyn <maybebazishin@gmail.com>
2025-08-09 08:11:58 +0300

    feat: assert

--- custom_engine/src/core/application.cpp
+++ custom_engine/src/core/application.cpp
@@ -2,11 +2,15 @@
 
 #include "application.hpp" // target
 
+#include "assert.hpp"
+
 namespace Engine
 {
 
     void CApplication::Run()
     {
+        ENGINE_ASSERT(0);
+
         for (;;)
         {
             std::cout << "Engine is running" << std::endl;
--- custom_engine/src/core/engine_entry_point.hpp
+++ custom_engine/src/core/engine_entry_point.hpp
@@ -12,12 +12,10 @@ extern Engine::CApplication &Engine::CreateApplication();
 int main(int argc, char **argv)
 {
     Engine::CLog::Init();
-    Engine::CLog::GetEngineLogger()->warn("Engine started.");
-    Engine::CLog::GetClientLogger()->info("Client started.");
 
     Engine::CApplication &app = Engine::CreateApplication();
 
-    // app.Run();
+    app.Run();
 
     return EXIT_SUCCESS;
 }
